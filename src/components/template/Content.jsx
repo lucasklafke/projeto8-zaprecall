5810338a4a0eeb1041ca2e6f21ccd4cfe5d396f6
Lucas klafke <lucasnkz11@gmail.com>
2022-03-18 09:12:36 -0300

    switch question states

--- src/components/template/Content.jsx
+++ src/components/template/Content.jsx
@@ -1,56 +1,86 @@
 import react from 'react'
 const questionsList = [
-    {
-        Id: 1,
-        status: "initial",
-        question: 'para que serve a função split?',
-        answer: 'divide uma String em uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array'
+    {   question:{
+            question: 'para que serve a função split?',
+            class: 'question-container'
+        },
+        answer: {
+            answer: 'divide uma String em uma lista ordenada de substrings, coloca essas substrings em um array e retorna o array',
+            class: 'answer-container'
+        }
+        
     },
     {
-        Id: 2,
-        status: "initial",
-        question: 'para que serve a função trim?',
-        answer: 'remove espaços em branco de ambos os lados de uma string, sem alterá-la'
+        question:{
+            question: 'para que serve a função trim?',
+            class: 'question-container'
+        },
+        answer: {
+            answer: 'remove espaços em branco de ambos os lados de uma string, sem alterá-la',
+            class: 'answer-container'
+        }
     },
     {
-        Id: 3,
-        status: "initial",
-        question: 'para que serve a função replace?',
-        answer: 'procura um valor ou uma expressão regular em uma string e retorna uma nova string com os valores substituídos. Não altera a string original. '
+        question:{
+            question: 'para que serve a função replace?',
+            class: ''
+        },
+        answer: {
+            answer: 'procura um valor ou uma expressão regular em uma string e retorna uma nova string com os valores substituídos. Não altera a string original.',
+            class: ''
+        }
     },
     {
-        Id: 4,
-        status: "initial",
-        question: 'Para que serve a função includes?',
-        answer: 'determina se um array contém um determinado elemento, retornando true ou false apropriadamente.'
+        question: {
+            question: 'Para que serve a função includes?',
+            class: 'question-container'
+        },
+        answer:{
+            answer: 'determina se um array contém um determinado elemento, retornando true ou false apropriadamente.',
+            class:'answer-container'
+        }
     }
 ]
 export default function Content(){
-    //const [state,setState] = react.useState(<Initial/>)
+    const [state,setState] = react.useState('initial')
     return (
         <main>
-            {questionsList.map(element => <Initial key={element.Id} Id={element.Id}/>)}
+            {questionsList.map((element,index) => <Question 
+            index={index} 
+            screenState={state} 
+            setState={setState} 
+            question={element.question.question}
+            answer={element.answer.answer}
+            />)}
         </main>
     )
 }
-
 function Question(props){
-    return (
-        <div className='question-container'>
-            <span>{props.question}</span>
-            <button ><img src="assets/setinha.png" alt="" /></button>
-        </div>
-    )
-}
-function Answer(props){
-
-}
-function Initial(props){
-    return (
-        <div className='initial-question-container'>
-            <span>pergunta {props.Id}</span>
-            <img src="assets/Vector.png" alt="" />
-        </div>
-    )
+    if(props.screenState === 'initial'){
+        return (
+            <div className="initial-question-container">
+                <span>pergunta </span>
+                <img onClick={()=> props.setState("question")} src="assets/Vector.png" alt="" />
+            </div>
+        )
+    } else if (props.screenState === 'question'){
+        return (
+            <div className="question-container">
+                <span>{props.question} </span>
+                <img onClick={()=> props.setState("answer")} src="assets/Vector.png" alt="" />
+            </div>
+        )
+    } else if (props.screenState === 'answer'){
+        return (
+            <div className="answer-container">
+                <span>{props.answer} </span>
+                <div>
+                    <button className='bt1'>Não lembrei</button>
+                    <button className='bt2'>Quase não lembrei</button>
+                    <button className='bt3'>Zap!</button>
+                </div>
+            </div>
+        )
+    } 
 }
 
